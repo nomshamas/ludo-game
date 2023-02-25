8d56855838f23b4a044b663cf1a300c7a6254ada
nomshamas <nomshamas@gmail.com>
2023-02-26 00:18:32 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const playAgain = document.querySelector('.newbtn');
+const playGame = document.querySelector('.rollbtn');
+const saveScore = document.querySelector('.holdbtn');
+
+playGame.addEventListener('click', function() {
+  
+});
+
+saveScore.addEventListener('click', function() {
+  
+});
+
+playAgain.addEventListener('click', function() {
+  
+});
+
+function randomNumber() {
+  return Math.floor(Math.random() * 6);
+}
